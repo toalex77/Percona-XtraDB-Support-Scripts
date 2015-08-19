6dcd5b333a2b2b5d4f161b97fda96f8e8cb73116
toalex77 <toalex77@users.noreply.github.com>
2015-08-19 11:10:30 +0200

    Create smtps_mail_send.sh

---
+++ smtps_mail_send.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export LANG=en_US.UTF-8
+from="mymail@mydomain.com"
+host="smtps://mail.mydomain.com:465"
+smtpsUser="smtpaccount@mydomain.com"
+smtpsPass="supersecretpassword"
+cat /dev/stdin | tr -d '\r' | /bin/mailx -S ssl-verify=ignore -S smtp-auth=login -S smtp="${host}" -S from="${from}" -S smtp-auth-user="${smtpsUser}" -S smtp-auth-password="${smtpsPass}" -S nss-config-dir=/etc/pki/nssdb -t "$@"
