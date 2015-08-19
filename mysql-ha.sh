461ba6c2e7de7e503bad44c558073550830084fe
toalex77 <toalex77@users.noreply.github.com>
2015-08-19 11:10:07 +0200

    Create mysql-ha.sh

---
+++ mysql-ha.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+. ./mysql-ha.conf.sh
+
+checkDBServers (){
+	local DB
+	nodesOk=()
+	nodesKO=()
+	for DB in "${DBNODES[@]}"; do
+		mysqladmin -h ${DB} ping > /dev/null 2>&1 && nodesOk+=( "${DB}" ) || nodesKO+=( "${DB}" )
+	done
+}
+
+checkArbServers (){
+	local Arb
+	arbsOk=()
+	arbsKO=()
+	for Arb in "${ARBNODES[@]}"; do
+		[ "$( /usr/bin/telnet "${Arb}" 4567 2>/dev/null < /dev/null | /usr/bin/wc -l )" -gt 1 ] && arbsOk+=( "${Arb}" ) || arbsKO+=( "${Arb}" )
+	done
+}
+
+pickRandomDB (){
+	pickRandomDB=$(( ( $RANDOM % ${#nodesOk[@]} ) ))
+	DBNODE=${nodesOk[${pickRandomDB}]}
+}
+
+if [ -n "${addDB}" ]; then
+	DBNODES+=( "${addDB[@]}" )
+fi
+
+if [ -n "${addARB}" ]; then
+	ARBNODES+=( "${addARB[@]}" )
+fi
+
+nodesOk=()
+arbsOk=()
+
+nodesKO=()
+arbsKO=()
+
+DBNODE=
+
+checkDBServers
