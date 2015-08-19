07a2c1c77852f2af06925ca05296e4cb868a6e81
toalex77 <toalex77@users.noreply.github.com>
2015-08-19 11:15:07 +0200

    Update smtps_mail_send.sh

--- smtps_mail_send.sh
+++ smtps_mail_send.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
+. ./smtps_mail_conf.sh
 from="mymail@mydomain.com"
-smtpsHost="smtps://mail.mydomain.com:465"
-smtpsUser="smtpaccount@mydomain.com"
-smtpsPass="supersecretpassword"
 cat /dev/stdin | tr -d '\r' | /bin/mailx -S ssl-verify=ignore -S smtp-auth=login -S smtp="${smtpsHost}" -S from="${from}" -S smtp-auth-user="${smtpsUser}" -S smtp-auth-password="${smtpsPass}" -S nss-config-dir=/etc/pki/nssdb -t "$@"
